ca22eef7bd24df9c1e380dcb396cfbbf6a13f85a
Denis <40057681+DSpoljar@users.noreply.github.com>
2021-03-03 17:58:46 +0100

    Changes in DisGeNetGraphExporter + HPOGraphExporter
    Updated export / Node methods in DisGeNetGraphExporter and implemented base methods in HPOGraphExporter.

--- src/biodwh2-disgenet/src/main/java/de/unibi/agbi/biodwh2/disgenet/etl/DisGeNetGraphExporter.java
+++ src/biodwh2-disgenet/src/main/java/de/unibi/agbi/biodwh2/disgenet/etl/DisGeNetGraphExporter.java
@@ -1,9 +1,11 @@
 package de.unibi.agbi.biodwh2.disgenet.etl;
 
+import com.fasterxml.jackson.databind.MappingIterator;
 import de.unibi.agbi.biodwh2.core.Workspace;
 import de.unibi.agbi.biodwh2.core.etl.GraphExporter;
 import de.unibi.agbi.biodwh2.core.exceptions.ExporterException;
 import de.unibi.agbi.biodwh2.core.exceptions.ExporterFormatException;
+import de.unibi.agbi.biodwh2.core.io.FileUtils;
 import de.unibi.agbi.biodwh2.core.io.obo.OboEntry;
 import de.unibi.agbi.biodwh2.core.io.obo.OboReader;
 import de.unibi.agbi.biodwh2.core.model.graph.Graph;
@@ -12,7 +14,8 @@ import de.unibi.agbi.biodwh2.disgenet.DisGeNetDataSource;
 import de.unibi.agbi.biodwh2.disgenet.model.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import java.io.FileReader;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
@@ -25,39 +28,68 @@ public class DisGeNetGraphExporter extends GraphExporter<DisGeNetDataSource>
 
     public DisGeNetGraphExporter(final DisGeNetDataSource dataSource)
     {
-        super(dataSource);
 
+        super(dataSource);
 
     }
 
     // Still WIP.
     @Override
-    protected boolean exportGraph(final Workspace workspace, final Graph graph) {
-        graph.setNodeIndexPropertyKeys("geneID", "geneSymbol");
+    protected boolean exportGraph(final Workspace workspace, final Graph graph)
+    {
+        graph.setNodeIndexPropertyKeys("GENE_ID", "DISEASE_ID");
+
+
+
 
         return true;
+
     }
 
     // Splitting different nodes and adding their respective properties (entries provisional):
 
-    private void exportGeneNode(final Graph graph, DisGeNetModel entry) {
+    private Node createGeneNode(final Graph graph, DisGeNetModel entry)
+    {
+        if (graph.findNode(entry.geneID).isEmpty())
+        {
+            Node geneNode = createNode(graph, "Gene");
+            geneNode.setProperty("ID", entry.geneID);
+            geneNode.setProperty("GENE_SYMBOL", entry.geneSymbol);
+            graph.update(geneNode);
+            return  geneNode;
+        }
+        else
+        {
+           Node currentNode = graph.findNode(entry.geneID);
+            return currentNode;
+        }
 
-        Node geneNode = createNode(graph, "geneId");
-        geneNode.setProperty("geneId", entry.geneID);
-        geneNode.setProperty("geneSymbol", entry.geneSymbol);
-        graph.update(geneNode);
+
     }
 
-    private void exportDiseaseNode(final Graph graph, DisGeNetModel entry) {
+    private Node createDiseaseNode(final Graph graph, DisGeNetModel entry)
+    {
+        if (graph.findNode(entry.diseaseID).isEmpty())
+        {
+            Node diseaseNode = createNode(graph, "Disease");
+            diseaseNode.setProperty("ID", entry.diseaseID);
+            diseaseNode.setProperty("DISEASE_NAME", entry.diseaseName);
+            diseaseNode.setProperty("DISEASE_TYPE", entry.diseaseType);
+            diseaseNode.setProperty("DISEASE_CLASS", entry.diseaseClass);
+            diseaseNode.setProperty("DISEASE_SEMANTICTYPE", entry.diseaseSemanticType);
+            graph.update(diseaseNode);
+            return diseaseNode;
+        }
+        else {
 
-        Node diseaseNode = createNode(graph, "diseaseId");
-        diseaseNode.setProperty("diseaseId", entry.diseaseID);
-        diseaseNode.setProperty("diseaseName", entry.diseaseName);
-        diseaseNode.setProperty("diseaseType", entry.diseaseType);
-        diseaseNode.setProperty("diseaseClass", entry.diseaseClass);
-        diseaseNode.setProperty("diseaseSemanticType", entry.diseaseSemanticType);
-        graph.update(diseaseNode);
+            Node currentNode = graph.findNode(entry.diseaseID);
+            return currentNode;
+
+        }
+
+
     }
+
 
 
 
--- src/biodwh2-hpo/src/main/java/de/unibi/agbi/biodwh2/hpo/etl/HPOGraphExporter.java
+++ src/biodwh2-hpo/src/main/java/de/unibi/agbi/biodwh2/hpo/etl/HPOGraphExporter.java
@@ -3,16 +3,47 @@ package de.unibi.agbi.biodwh2.hpo.etl;
 import de.unibi.agbi.biodwh2.core.Workspace;
 import de.unibi.agbi.biodwh2.core.etl.GraphExporter;
 import de.unibi.agbi.biodwh2.core.exceptions.ExporterException;
+import de.unibi.agbi.biodwh2.core.exceptions.ExporterFormatException;
+import de.unibi.agbi.biodwh2.core.io.obo.OboEntry;
+import de.unibi.agbi.biodwh2.core.io.obo.OboReader;
 import de.unibi.agbi.biodwh2.core.model.graph.Graph;
+import de.unibi.agbi.biodwh2.core.model.graph.Node;
 import de.unibi.agbi.biodwh2.hpo.HPODataSource;
 
-public class HPOGraphExporter extends GraphExporter<HPODataSource> {
-    public HPOGraphExporter(final HPODataSource dataSource) {
+import java.io.IOException;
+
+public class HPOGraphExporter extends GraphExporter<HPODataSource>
+{
+    public HPOGraphExporter(final HPODataSource dataSource)
+    {
+
         super(dataSource);
+
     }
 
     @Override
-    protected boolean exportGraph(final Workspace workspace, final Graph graph) {
+    protected boolean exportGraph(final Workspace workspace, final Graph graph)
+    {
+        graph.setNodeIndexPropertyKeys("id");
+        try {
+            OboReader reader = new OboReader(dataSource.resolveSourceFilePath(workspace, "ho.obo"), "UTF-8");
+            for (OboEntry entry : reader)
+                if (entry.getName().equals("Term"))
+                    exportEntry(graph, entry);
+        } catch (IOException e) {
+            throw new ExporterFormatException("Failed to export ho.obo", e);
+        }
         return true;
+
+    }
+
+    private void exportEntry(final Graph graph, final OboEntry entry) {
+        if (entry.containsKey("is_obsolete") && "true".equalsIgnoreCase(entry.getFirst("is_obsolete")))
+            return;
+        Node node = createNode(graph, "Term");
+        node.setProperty("DATABASE", entry.getFirst("disease-db"));
+        node.setProperty("ID", entry.getFirst("HPO-ID"));
+        node.setProperty("NAME", entry.getFirst("disease-name"));
+        node.setProperty("IDENTIFIER", entry.getFirst("disease-identifier"));
     }
 }
