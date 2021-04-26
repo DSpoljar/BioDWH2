14531c45d7a535664e7f08886448e92418580b1c
Denis <40057681+DSpoljar@users.noreply.github.com>
2021-04-26 15:44:43 +0200

    Small changes in HPOGraphExporter
    Some prerequisite structures for HPOGraphExporter

--- src/biodwh2-hpo/src/main/java/de/unibi/agbi/biodwh2/hpo/etl/HPOGraphExporter.java
+++ src/biodwh2-hpo/src/main/java/de/unibi/agbi/biodwh2/hpo/etl/HPOGraphExporter.java
@@ -1,19 +1,26 @@
 package de.unibi.agbi.biodwh2.hpo.etl;
 
+import com.fasterxml.jackson.databind.MappingIterator;
 import de.unibi.agbi.biodwh2.core.Workspace;
 import de.unibi.agbi.biodwh2.core.etl.GraphExporter;
 import de.unibi.agbi.biodwh2.core.exceptions.ExporterException;
 import de.unibi.agbi.biodwh2.core.exceptions.ExporterFormatException;
+import de.unibi.agbi.biodwh2.core.io.FileUtils;
 import de.unibi.agbi.biodwh2.core.io.obo.OboEntry;
 import de.unibi.agbi.biodwh2.core.io.obo.OboReader;
 import de.unibi.agbi.biodwh2.core.model.graph.Graph;
 import de.unibi.agbi.biodwh2.core.model.graph.Node;
 import de.unibi.agbi.biodwh2.hpo.HPODataSource;
+import de.unibi.agbi.biodwh2.hpo.model.HPOModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
 public class HPOGraphExporter extends GraphExporter<HPODataSource>
 {
+    private static final Logger LOGGER = LoggerFactory.getLogger(HPOGraphExporter.class);
+
     public HPOGraphExporter(final HPODataSource dataSource)
     {
 
@@ -21,20 +28,54 @@ public class HPOGraphExporter extends GraphExporter<HPODataSource>
 
     }
 
+    final String[] txtFiles = new String[]{
+
+            "phenotypes_to_genes.txt"
+
+    };
+
     @Override
     protected boolean exportGraph(final Workspace workspace, final Graph graph)
     {
+        int counter = 0;
+
         graph.setNodeIndexPropertyKeys("id");
         try {
             OboReader reader = new OboReader(dataSource.resolveSourceFilePath(workspace, "hp.obo"), "UTF-8");
             for (OboEntry entry : reader)
                 if (entry.getName().equals("Term"))
                     exportEntry(graph, entry);
+                     counter++;
+                     if (counter % 1000 == 0)
+                     LOGGER.info("Progress: " + counter);
         } catch (IOException e) {
             throw new ExporterFormatException("Failed to export hp.obo", e);
         }
-        return true;
 
+
+         counter = 0;
+
+        for (String file : txtFiles)
+        {
+            LOGGER.info("Exporting " + file);
+            try {
+
+                final MappingIterator<HPOModel> iterator = FileUtils.openCsv(workspace, dataSource,
+                                                                             file,
+                                                                             HPOModel.class);
+
+                // TODO: Add nodes and  edges for phenotype_to_genes
+
+                    counter++;
+                    if (counter % 1000 == 0)
+                        LOGGER.info("Progress: " + counter);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return true;
     }
 
     private void exportEntry(final Graph graph, final OboEntry entry) {
